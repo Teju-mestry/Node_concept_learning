10fd145dcb85ce0114a49b640f4568ef40d6a646
tejashree suryakant mestry <tejashreemestry40@gmail.com>
2020-07-16 16:11:12 +0530

    Added function to get messages from server.js where we store list of messages

--- demo_app_chat_client/server.js
+++ demo_app_chat_client/server.js
@@ -2,6 +2,15 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname))
+
+var messages = [
+    { name: 'Tejashree', message: 'Hi' },
+    { name: 'Malini', message: 'Hello' }
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
 var server = app.listen(3000, () => {
     console.log('Server is running on port', server.address().port)
 })
